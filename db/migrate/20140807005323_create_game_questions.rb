16ef6a978bb93435e4497c853ed4f708f1b4f43b
Jeff Q <jqiu1998@yahoo.com>
2014-08-08 19:40:14 -0400

    Removed the GameQuestion model and added game_id to Question

--- db/migrate/20140807005323_create_game_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateGameQuestions < ActiveRecord::Migration
-  def change
-    create_table :game_questions do |t|
-      t.integer :question_id
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140807233903_create_vocab_questions.rb
+++ db/migrate/20140807233903_create_vocab_questions.rb
@@ -1,6 +1,7 @@
 class CreateVocabQuestions < ActiveRecord::Migration
   def change
     create_table :vocab_questions do |t|
+      t.integer :game_id
       t.integer :correct_answer
       t.string :word_location
       t.string :player_answer
