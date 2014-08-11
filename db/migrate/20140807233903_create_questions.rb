c4f895b8f41e5cbf85964cba3ae6b90ff4990584
Nick Marden <nick@marden.org>
2014-08-10 21:24:32 -0400

    These two methods are questions you can ask about a Question object, so we make them instance methods on the Question class

--- db/migrate/20140807233903_create_questions.rb
+++ db/migrate/20140807233903_create_questions.rb
@@ -8,11 +8,4 @@ class CreateQuestions < ActiveRecord::Migration
     end
   
   end
-  def correct_word
-    answers.where(is_correct_answer:true).first
-  end
-
-  def incorrect_words
-    answers.where(is_correct_answer.false).reorder(:word_location)
-  end
 end
